3da4fdc068f654938342f4a92e77679e9dc9649b
Letícia <leticialimaferreira@gmail.com>
2021-11-21 19:52:33 -0300

    Paginas da aplicãção delivery, com metodos funcionando

---
+++ src/app/acompanhar-pedido/acompanhar-pedido.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acompanhar-pedido',
+  templateUrl: './acompanhar-pedido.component.html',
+  styles: [
+  ]
+})
+export class AcompanharPedidoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
